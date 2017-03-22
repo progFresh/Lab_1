3e50ef82e076f8514ad1685b13384b3d34ace5e0
Сергей Полозов <fresher40@gmail.com>
2017-03-23 01:23:07 +0300

    добавлено выделение памяти

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;                                                                                            //include conio.h +_getch();
 
+template < typename T >
+void fillArray ( T ** array , short N ) {
+
+}
+
 int main() {
-    string equationType;
+    short equationType;
     string readForQuit;
     string variableType;
     bool canContinue;
@@ -22,7 +27,7 @@ int main() {
             cout << "неверный ввод" << endl;
             canContinue = false;
         } else {
-            if (equationType != "2" && equationType != "3" && equationType !="4") {
+            if (equationType != 2 && equationType != 3 && equationType != 4) {
                 cout << "не могу решить задачу такого порядка :)" << endl;
                 canContinue = false;
             }
@@ -43,8 +48,51 @@ int main() {
                 }
             }
         }
-        //TODO
 
+        //TODO
+        if (canContinue) {
+            // создание
+            if (variableType == "float") {
+                // FLOAT
+                // Инициализация
+                float **arrayFloat;
+                arrayFloat = new float * [equationType];
+                for (int i = 0; i < equationType; i++) {
+                    arrayFloat[i] = new float [equationType+1];
+                }
+                // Удаление:
+                for (int i = 0; i < equationType; i++) {
+                    delete [] arrayFloat[i];
+                }
+                delete [] arrayFloat;
+            } else if (variableType == "short") {
+                // SHORT
+                // Инициализация
+                short **arrayShort;
+                arrayShort = new short * [equationType];
+                for (int i = 0; i < equationType; i++) {
+                    arrayShort[i] = new short [equationType+1];
+                }
+                // Удаление:
+                for (int i = 0; i < equationType; i++) {
+                    delete [] arrayShort[i];
+                }
+                delete [] arrayShort;
+            } else if (variableType == "double") {
+                // DOUBLE
+                // Инициализация
+                double **arrayDouble;
+                arrayDouble = new double * [equationType];
+                for (int i = 0; i < equationType; i++) {
+                    arrayDouble[i] = new double [equationType+1];
+                }
+                // Удаление:
+                for (int i = 0; i < equationType; i++) {
+                    delete [] arrayDouble[i];
+                }
+                delete [] arrayDouble;
+            }
+        }
         cout << "для выхода введите \'y\'" << endl;
     } while(cin >> readForQuit && readForQuit != "y");
 
